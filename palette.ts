a84a7fe18782f97623d1e7bd55f43aa241cb6b5a
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2024-09-27 01:46:07 +0330

    Install and configure "daisyUI" library + dark/light themes

---
+++ palette.ts
@@ -0,0 +1,63 @@
+import type {CustomTheme} from 'daisyui'
+import themes from 'daisyui/src/theming/themes'
+// import type {DefaultColors} from 'tailwindcss/types/generated/colors' // Don't `import colors from 'tailwindcss/colors'`. Just `import type` from 'tailwindcss' package. Otherwise, you need to put it in `dependencies` (instead of `devDependencies`)
+
+// type Gray = DefaultColors['gray']
+
+/**
+ * @see [How to customize an existing theme?](https://daisyui.com/docs/themes/#-7)
+ * @see [List of all daisyUI color names](https://daisyui.com/docs/colors/#-2)
+ * @see [How to extend default daisyUI color
+ *   names?](https://github.com/saadeghi/daisyui/discussions/653#discussioncomment-2438428)
+ */
+const PALETTES = {
+  light: {
+    ...themes.light,
+    primary: '#1976d2',
+    // // Other theme-colors should be provided as CSS variables: // See the above link.
+    // '--secondary-text': hexColorToRgbString('#9ca3af' satisfies Gray['400']),
+  },
+  dark: {
+    ...themes.dark,
+    primary: '#a5c8ff',
+    // // Other theme-colors should be provided as CSS variables: // See the above link.
+    // '--secondary-text': hexColorToRgbString('#9ca3af' satisfies Gray['400']),
+  },
+} satisfies CustomTheme
+
+export default PALETTES
+
+// /**
+//  * @example
+//  * hexColorToRgbString('#123546') // => '18 52 86'
+//  */
+// function hexColorToRgbString(hex: string) {
+//   const r = parseInt(hex.slice(1, 3), 16)
+//   const g = parseInt(hex.slice(3, 5), 16)
+//   const b = parseInt(hex.slice(5, 7), 16)
+//   return [r, g, b].join(' ')
+// }
+
+export type ColorName =
+  | 'primary'
+  | 'secondary'
+  | 'accent'
+  | 'neutral'
+  | 'base-100'
+  | 'base-200'
+  | 'base-300'
+  | 'info'
+  | 'success'
+  | 'warning'
+  | 'error'
+export type ContentColorName =
+  | 'primary-content'
+  | 'secondary-content'
+  | 'accent-content'
+  | 'neutral-content'
+  | 'base-content'
+  | 'info-content'
+  | 'success-content'
+  | 'warning-content'
+  | 'error-content'
+export type AllColorName = ColorName | ContentColorName // Should be `keyof ValueOf<CustomTheme>`. But it's just `string` now!
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
+import type {Config as DaisyUIConfig} from 'daisyui'
+import daisyui from 'daisyui'
 import type {Config} from 'tailwindcss'
+import PALETTES from './palette'
 
 const config: Config = {
   content: [
@@ -6,14 +9,31 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: [
+    'variant',
+    ['@media(prefers-color-scheme:dark){&:not([data-theme=light] *)}', '&:is([data-theme=dark] *)'],
+  ],
   theme: {
     extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
+      /**
+       * Transform daisyUI css-variables to tailwind colors (specially, handle the opacity of color).
+       * @see https://github.com/saadeghi/daisyui/discussions/653#discussioncomment-2438428
+       * @example
+       * {background: 'rgb(var(--background) / <alpha-value>)', shaded: 'rgb(var(--shaded) / <alpha-value>)'}
+       */
+      colors: Object.fromEntries(
+        Object.keys(PALETTES.light)
+          .filter((colorNameOrCssVar) => colorNameOrCssVar.startsWith('--'))
+          // https://tailwindcss.com/docs/customizing-colors#using-css-variables
+          .map((colorNameAsCssVar) => [colorNameAsCssVar.slice(2), `rgb(var(${colorNameAsCssVar}) / <alpha-value>)`]),
+      ),
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    /** @see [How to customize an existing theme?](https://daisyui.com/docs/themes/#-7) */
+    themes: [{light: PALETTES.light}, {dark: PALETTES.dark}] as const,
+    logs: false,
+  } satisfies DaisyUIConfig,
 }
 export default config
