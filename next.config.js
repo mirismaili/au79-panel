77dbbc4c04216a8e09db82c575293f073b241f22
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2024-09-27 15:09:08 +0330

    Apply Prettier + ESLint on whole project

--- next.config.js
+++ next.config.js
@@ -3,6 +3,5 @@ const nextConfig = {
   // Typescript and ESLint errors are checked in standalone steps during CI/CD pipelines, so it's not needed to check them here:
   typescript: {ignoreBuildErrors: true},
   eslint: {ignoreDuringBuilds: true},
-};
-
-export default nextConfig;
+}
+export default nextConfig
