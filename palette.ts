396fecd96fea29e1fb8395a031b74d8385fa826f
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2024-09-27 16:29:29 +0330

    CSS: Configure thin-scrollbar

--- palette.ts
+++ palette.ts
@@ -1,8 +1,8 @@
 import type {CustomTheme} from 'daisyui'
 import themes from 'daisyui/src/theming/themes'
-// import type {DefaultColors} from 'tailwindcss/types/generated/colors' // Don't `import colors from 'tailwindcss/colors'`. Just `import type` from 'tailwindcss' package. Otherwise, you need to put it in `dependencies` (instead of `devDependencies`)
+import type {DefaultColors} from 'tailwindcss/types/generated/colors' // Don't `import colors from 'tailwindcss/colors'`. Just `import type` from 'tailwindcss' package. Otherwise, you need to put it in `dependencies` (instead of `devDependencies`)
 
-// type Gray = DefaultColors['gray']
+type Gray = DefaultColors['gray']
 
 /**
  * @see [How to customize an existing theme?](https://daisyui.com/docs/themes/#-7)
@@ -13,30 +13,34 @@ import themes from 'daisyui/src/theming/themes'
 const PALETTES = {
   light: {
     ...themes.light,
-    primary: '#1976d2',
+    'primary': '#1976d2',
     // // Other theme-colors should be provided as CSS variables: // See the above link.
     // '--secondary-text': hexColorToRgbString('#9ca3af' satisfies Gray['400']),
+    '--scrollbar-color': hexColorToRgbString('#d1d5db' satisfies Gray['300']),
+    '--scrollbar-color-hover': hexColorToRgbString('#9ca3af' satisfies Gray['400']),
   },
   dark: {
     ...themes.dark,
-    primary: '#a5c8ff',
+    'primary': '#a5c8ff',
     // // Other theme-colors should be provided as CSS variables: // See the above link.
     // '--secondary-text': hexColorToRgbString('#9ca3af' satisfies Gray['400']),
+    '--scrollbar-color': hexColorToRgbString('#4b5563' satisfies Gray['600']),
+    '--scrollbar-color-hover': hexColorToRgbString('#6b7280' satisfies Gray['500']),
   },
 } satisfies CustomTheme
 
 export default PALETTES
 
-// /**
-//  * @example
-//  * hexColorToRgbString('#123546') // => '18 52 86'
-//  */
-// function hexColorToRgbString(hex: string) {
-//   const r = parseInt(hex.slice(1, 3), 16)
-//   const g = parseInt(hex.slice(3, 5), 16)
-//   const b = parseInt(hex.slice(5, 7), 16)
-//   return [r, g, b].join(' ')
-// }
+/**
+ * @example
+ * hexColorToRgbString('#123546') // => '18 52 86'
+ */
+function hexColorToRgbString(hex: string) {
+  const r = parseInt(hex.slice(1, 3), 16)
+  const g = parseInt(hex.slice(3, 5), 16)
+  const b = parseInt(hex.slice(5, 7), 16)
+  return [r, g, b].join(' ')
+}
 
 export type ColorName =
   | 'primary'
