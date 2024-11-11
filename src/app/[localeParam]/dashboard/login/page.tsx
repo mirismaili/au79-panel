6c6b51a53a0da7479b3fce3e4033d41ac664b365
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2024-11-11 11:00:23 +0330

    Passkey v0.1

--- src/app/[localeParam]/dashboard/login/page.tsx
+++ src/app/[localeParam]/dashboard/login/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import regionPhonePrefixes from '@/app/[localeParam]/dashboard/login/region-phone-prefixes'
+import {startAuthentication, startRegistration} from '@simplewebauthn/browser'
+import type {PublicKeyCredentialCreationOptionsJSON} from '@simplewebauthn/types'
 import {KeyRound, Phone} from 'lucide-react'
 import {useForm} from 'react-hook-form'
 
@@ -14,7 +16,28 @@ export default function Home() {
     <main className="flex grow flex-col">
       <div className="mx-auto mt-24 w-full rounded-md p-6 shadow-md lg:max-w-lg">
         <h1 className="text-center text-3xl font-semibold text-primary">Login</h1>
-        <form className="flex flex-grow flex-col gap-4" onSubmit={handleSubmit((data) => console.log(data))}>
+        <form
+          className="flex flex-grow flex-col gap-4"
+          onSubmit={handleSubmit(async (data) => {
+            const options = (await fetch(new URL('/auth/register', 'http://localhost:7979')).then((res) =>
+              res.json(),
+            )) as PublicKeyCredentialCreationOptionsJSON
+
+            console.log('options', options)
+            const agentAuthenticationResponse = await startRegistration({optionsJSON: options})
+
+            const verificationResponse = await fetch(new URL('/auth/register', 'http://localhost:7979'), {
+              method: 'POST',
+              credentials: 'include',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(agentAuthenticationResponse),
+            })
+
+            console.log(verificationResponse)
+          })}
+        >
           <label
             dir="ltr"
             className={`input input-bordered flex w-full items-center gap-2 ${errors.phone ? 'input-error' : 'input-primary'}`}
